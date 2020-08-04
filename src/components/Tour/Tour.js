ff2fc58c42c8ef2a1b76b53e7c81f989e5a85d81
dileep <dileep.kumar1228@gmail.com>
2020-08-04 19:40:54 +0530

    first commit

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import "./Tour.scss";
+
+class Tour extends Component {
+    state ={
+        showInfo :false
+    };
+    handleInfo = ()=>{
+        this.setState({
+            showInfo:!this.state.showInfo
+        })
+    }
+    render() {
+        const {city,img,name,info} = this.props.tour;
+        return (
+            <article className = "tour">
+                <div className="ima-container">
+                    <img src={img} alt=""/>
+                    <span className="close-btn">
+                        <i className="fas fa-window-close" />
+                    </span>
+                </div>
+                <div className="tour-info">
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>info 
+                        <span onClick={this.handleInfo}>
+                            <i className="fas fa-caret-square-down"></i>
+                        </span>
+                    </h5>
+                    {this.state.showInfo && <p>{info}</p>}
+                    
+                </div>
+            </article>
+        );
+    }
+}
+
+export default Tour;
